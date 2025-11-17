ce47e6c03fe5883c59181894279f884f392cf3d1
mooneybb <mooneybb@tcd.ie>
2025-11-17 11:50:38 +0000

    edited file to allow for characters to be displayed nicely

--- gpt.py
+++ gpt.py
@@ -19,12 +19,19 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('input_shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
+# display vocab_size for easier reporting:
+print(f"Unique characters {vocab_size}:")
+for i, ch in enumerate(chars):
+    print(repr(ch), end=", ")
+    if (i+1) % 10 == 0:
+        print()
+print()
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
@@ -212,6 +219,7 @@ for iter in range(max_iters):
 
     # sample a batch of data
     xb, yb = get_batch('train')
+    # may need to add to device shit
 
     # evaluate the loss
     logits, loss = model(xb, yb)
